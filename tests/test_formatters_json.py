0940d79eca59f1b5a295386fa9e815df0a81b52e
Iulia Lesnikova <lesnikova.j00@gmail.com>
2024-01-10 19:52:45 +0500

    fix linter

--- tests/test_formatters_json.py
+++ tests/test_formatters_json.py
@@ -1,14 +1,21 @@
-import pytest
 from gendiff.gendiff import generate_diff
 
 
 def test_json():
     paths = [
-      ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_for_json.json'),
-      ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'tests/fixtures/result_for_json.json')
+        (
+            "tests/fixtures/file1.json",
+            "tests/fixtures/file2.json",
+            "tests/fixtures/result_for_json.json",
+        ),
+        (
+            "tests/fixtures/file1.yml",
+            "tests/fixtures/file2.yml",
+            "tests/fixtures/result_for_json.json",
+        ),
     ]
     for p in paths:
-        res = generate_diff(p[0], p[1], 'json')
+        res = generate_diff(p[0], p[1], "json")
         with open(p[2]) as file:
             answ = file.read()
             assert res == answ
--- tests/test_formatters_plain.py
+++ tests/test_formatters_plain.py
@@ -1,14 +1,21 @@
-import pytest
 from gendiff.gendiff import generate_diff
 
 
 def test_plain():
     paths = [
-        ('tests/fixtures/file1_recursive.json', 'tests/fixtures/file2_recursive.json', 'tests/fixtures/result_for_plain'),
-        ('tests/fixtures/file1_recursive.yaml', 'tests/fixtures/file2_recursive.yaml', 'tests/fixtures/result_for_plain')
-  ]
+        (
+            "tests/fixtures/file1_recursive.json",
+            "tests/fixtures/file2_recursive.json",
+            "tests/fixtures/result_for_plain",
+        ),
+        (
+            "tests/fixtures/file1_recursive.yaml",
+            "tests/fixtures/file2_recursive.yaml",
+            "tests/fixtures/result_for_plain",
+        ),
+    ]
     for p in paths:
-        res = generate_diff(p[0], p[1], 'plain')
+        res = generate_diff(p[0], p[1], "plain")
         with open(p[2]) as file:
             answ = file.read()
             assert res == answ
